3ca2caf6098df3f4ef5b05376b58c87af1a254e0
unknown <jeanfranco_virgo@hotmail.com>
2019-10-07 23:54:56 -0500

    Mi primer commit

---
+++ Hello_Wolrd.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(){
+	
+	printf("\nHello World");
+
+	system("pause");
+	return 0;
+}
---
+++ define_for.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define i 5
+
+void main(){
+	
+	int n=0;
+
+	for (n;n<=i;n++){
+
+		printf("\nHello World");
+
+	}
+	
+	system("pause");
+	return 0;
+}
